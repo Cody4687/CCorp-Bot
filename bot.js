2dfd86299edf1624ee26fa5f267e2879b5d3607b
Cody <38409768+Cody4687@users.noreply.github.com>
2019-09-29 17:02:29 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,12 +1,12 @@
-const Discord = require('discord.js');
 const mineflayer = require('mineflayer');
-const client = new Discord.Client();
 const navigatePlugin = require('mineflayer-navigate')(mineflayer);
+const vec3 = require('vec3')
 var colors = require('colors');
 var config = require("./config.json")
 var prefix = config.prefix
 
-/*
+/* 
+
 0b0t Chat Colors
 ! yellow
 # pink
@@ -24,7 +24,7 @@ var options = {
     username: config.email,
     version: "1.12.2",
     verbose: "true",
-    password: config.password,
+    password: config.password
 };
 
 var bot = mineflayer.createBot(options);
@@ -33,10 +33,6 @@ navigatePlugin(bot);
 
 function bindEvents(bot) {
 
-    function antiafk() {
-        bot.navigate.to(bot.entity.position.offset(-1, 0, -1));
-        bot.navigate.to(bot.entity.position.offset(1, 0, 1));
-    }
 
     function RussianRoulette() {
         let math = Math.floor(Math.random() * 7)
@@ -48,10 +44,11 @@ function bindEvents(bot) {
     }
 
     let responses = ['# Yes.', '# No.', '# Not Likely.', '# Very Likely.', '# Unsure.', '# It is certain.']
-function Ball() {
-    let math = Math.floor(Math.random() * responses.length)
-    return(responses[math])
-}
+
+    function Ball() {
+        let math = Math.floor(Math.random() * responses.length)
+        return (responses[math])
+    }
 
     function chat(b, c) {
         bot.chat(b)
@@ -59,20 +56,48 @@ function Ball() {
     }
 
     function isAllowed(username) {
-        array = ['Cody4687', 'DecendiumXD', 'Kory_B_Cervado', 'Garmadon_Prime', 'Mr_MagicMan', 'NiuNiu0326', 'heccinsucc', 'Zachere', 'DouglasJack', 'MireDieYoung', 'Distaf']
+        array = ['Cody4687', 'Garmadon_Prime', 'heccinsucc', 'Zachere', 'DouglasJack', 'Distaf', 'HiImHappy', 'zxow']
         if (array.includes(username)) {
             chat(`> Accepted tpa for ${username}.`, `Accepted tpa for ${username}.`.green)
             return (username)
-        } else return chat(`< You're not on the list, ${username}!`, `${username} attempted to tpa.`.red)
+        } else return chat(`< ${username} is not on the list!`, `${username} attempted to tpa.`.red)
     }
+
+    function isCody(username) {
+        array = ['Cody4687']
+        if (array.includes(username)) {
+            return (username)
+        } else return chat(`< You're not on the list, ${username}!`, `${username} attempted an admin command.`.red)
+    }
+
+    function isCCorp(username) {
+        array = ['Cody4687', 'Garmadon_Prime', 'heccinsucc', 'Zachere', 'DouglasJack', 'Distaf', 'HiImHappy', 'zxow', 'FINZO', 'decendium_', 'fallenangle1110', 'mrcurry45']
+        if (array.includes(username)) {
+            return(`true`)
+            return (username)
+        } else return(`false`)
+    }
+
 
     bot.on('login', function () {
         console.log(`Minecraft Bot Online!`.rainbow)
-        setTimeout(function () {
-            antiafk();
-        }, 5000);
-
     });
+
+    
+    bot.on('login', () => {
+        var posone = new vec3();
+        var postwo = new vec3();
+        function one() {
+            bot.navigate.to(posone)
+            setTimeout(two, 20000)
+        }
+        function two() {
+            bot.navigate.to(postwo)
+            setTimeout(one, 20000)
+        }
+        setTimeout(one, 10000)
+    });
+
 
     bot.on('error', function (err) {
         console.log('Error attempting to reconnect: ' + err.errno + '.');
@@ -98,18 +123,10 @@ function Ball() {
         navigatePlugin(bot);
     }
 
-    client.on('ready', () => {
-        console.log(`Discord Bot Online!`.rainbow)
-    });
-
-    bot.on('chat', function (username, message) {
-        if (username === bot.username) return;
-        client.channels.get(config.channelID).send(`<${username}> ${message}`)
-    });
-
     bot.on('chat', (username, message) => {
         const args = message.split(' ')
         const cmd = message.split(' ')[0]
+
 
         if (cmd === `${prefix}accept`) {
             bot.chat(`/tpy ${isAllowed(username)}`)
@@ -119,16 +136,29 @@ function Ball() {
             bot.chat(`/tpy ${isAllowed(username)}`)
         }
 
+
         if (cmd === `${prefix}russianroulette`) {
-            chat(`${RussianRoulette()}`, `${username} used ${prefix}russianroulette!`.yellow)
+            chat(`${RussianRoulette()}`, `${username} used ${prefix}russianroulette.`.yellow)
         }
 
         if (cmd === `${prefix}8ball`) {
-            chat(`${Ball()}`, `${username} used ${prefix}8ball!`.yellow)
+            chat(`${Ball()}`, `${username} used ${prefix}8ball.`.yellow)
         }
 
         if (cmd === `${prefix}tpa`) {
             chat(`/tpa Cody4687`, `Bot tpa'd to Cody4687.`.green)
+        }
+        
+        if (cmd === `${prefix}test`) {
+            console.log(bot.players)
+        }
+
+        if (cmd === `${prefix}uuid`) {
+            chat(`, Your uuid is ${bot.players[username].uuid}!`, `${username} used ${prefix}uuid.`.yellow)
+        }
+
+        if (cmd === `${prefix}ping`) {
+            chat(`, Your ping is ${bot.players[username].ping}!`, `${username} used ${prefix}ping.`.yellow)
         }
 
         if (cmd === `${prefix}coinflip`) {
@@ -144,7 +174,6 @@ function Ball() {
             return (chat(`, ${coinflip()}`, `${username} used ${prefix}coinflip.`.yellow))
         }
 
-        
+
     })
 }
-client.login(config.token)
